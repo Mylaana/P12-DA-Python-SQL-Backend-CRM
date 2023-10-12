820fb09dee41e54388f6af621ac2a68462bddb99
Mylaana <alexandre.thore34@gmail.com>
2023-10-12 10:20:07 +0200

    server running with mysql

--- EpicEvents/settings.py
+++ EpicEvents/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import configparser
+import os
+
+db_config_path = os.path.join(os.path.dirname(__file__), 'db_config.ini')
+
+#loading db_config info
+config = configparser.ConfigParser()
+config.read(db_config_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,16 +75,22 @@ TEMPLATES = [
     },
 ]
 
+
+
 WSGI_APPLICATION = 'EpicEvents.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# configuring database as mysql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config.get('mysql', 'database'),
+        'USER': config.get('mysql', 'user'),
+        'PASSWORD': config.get('mysql', 'password'),
+        'HOST': 'localhost',  # should match the mysql server adress
+        'PORT': '3306',
     }
 }
 
