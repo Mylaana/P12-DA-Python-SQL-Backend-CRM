c27927991f3812fcdb59a1147b230e7d16f53d65
Mylaana <alexandre.thore34@gmail.com>
2023-10-27 13:51:32 +0200

    writes and loads credentials in json

--- UserProfile/management/commands/login.py
+++ UserProfile/management/commands/login.py
@@ -1,30 +1,61 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
 from getpass import getpass
 import requests
-
+import json
+from EpicEvents.settings import BASE_DIR
 
 AUTH_URL = "http://localhost:8000/token-auth/"
+TOKEN_FILENAME = "credentials.json"
 
 class Command(BaseCommand):
 
-    help = 'Affiche la liste des utilisateurs enregistrés dans la base de données'
-
     def handle(self, *args, **options):
+        """handles 'login' command"""
+
+        #check if token exists
+        #check_if_token()
+        auth_data = self.request_get_token()
+        if auth_data['token'] is not None:
+            print(f"Vous êtes authentifié, bienvenue {auth_data['username']}")
+            self.write_token(auth_data)
+
+
+    def request_get_token(self):
+        """asks user for credentials and returns token as string
+        returns none if could not authenticate"""
         username = input("enter username:")
         password = getpass("enter password:")
 
         auth_data = {
             'username': username,
-            'password': password
+            'password': password,
+            'token': None
             }
-        
-        response = requests.post(url=AUTH_URL, data=auth_data)
-        #user = authenticate(username=username, password=password)
-        print (response.status_code)
-        if response.status_code == 200:
-            print(f"token : {response.json().get('token')}")
 
-    def request_get_token(self):
-        pass
+        response = requests.post(url=AUTH_URL, data=auth_data, timeout=5000)
+
+        if response.status_code != 200:
+            print("Authentification impossible")
+            print(f"response status code: {response.status_code}")
+            return auth_data
+
+        auth_data['token'] = response.json().get('token')
+        return auth_data
+
+    def write_token(self, auth_data):
+        """writes token in credential json file"""
+
+        #removes password from data to be written
+        write_data = auth_data
+        write_data['password'] = ''
+
+        with open(str(BASE_DIR) + "/" + TOKEN_FILENAME, 'w') as json_file:
+            json.dump(auth_data, json_file, indent=4)
+
+    def load_token(self):
+        """loads token from credential json file"""
+
+        with open(TOKEN_FILENAME, 'r') as json_file:
+            loaded_data = json.load(json_file)
+
+        return loaded_data
--- UserProfile/views.py
+++ UserProfile/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
 from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
