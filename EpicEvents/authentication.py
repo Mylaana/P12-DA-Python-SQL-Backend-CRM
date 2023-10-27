52749439e278abff8d19f10848ce2af85ff25902
Mylaana <alexandre.thore34@gmail.com>
2023-10-27 14:16:41 +0200

    refactoring

--- EpicEvents/authentication.py
+++ EpicEvents/authentication.py
@@ -2,6 +2,20 @@ from getpass import getpass
 import json
 import requests
 from EpicEvents.settings import BASE_DIR
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    """handles the token generation"""
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        return Response({'error': 'Login failed'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 AUTH_URL = "http://localhost:8000/token-auth/"
--- EpicEvents/urls.py
+++ EpicEvents/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from UserProfile.views import CustomObtainAuthToken
+from .authentication import CustomObtainAuthToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
