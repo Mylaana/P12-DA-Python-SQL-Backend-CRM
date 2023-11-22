8f6cb6857b8a7704693d1ff0752c1ed9b31b3526
Mylaana <alexandre.thore34@gmail.com>
2023-11-22 14:24:28 +0100

    refactoring code for request_command redesign

--- Client/management/commands/client.py
+++ Client/management/commands/client.py
@@ -3,10 +3,11 @@ from django.core.management.base import BaseCommand
 from Client.models import Client
 from UserProfile.models import UserProfile
 from EpicEvents.utils import input_validated, request_commands, print_command_result
-from EpicEvents.utils import get_ee_client_id, get_ee_contact_id, get_ee_contact_name
-
+from EpicEvents.utils import get_object_from_field_name, get_date_time_from_user
+from EpicEvents.utils import ERROR_MESSAGE
 
 CLIENT_FIELD_LIST = [
+    'ee_contact',
     'name',
     'siren',
     'client_contact_name',
@@ -16,7 +17,7 @@ CLIENT_FIELD_LIST = [
     ]
 
 CLIENT_DESCRIPTION = {
-    'ee_contact_name': "Epic Events - Nom du contact: ",
+    'ee_contact': "Epic Events - Nom du contact: ",
     'ee_contact_id': "Epic Events - ID du contact: ",
     'name': "Client - Nom: ",
     'siren': "Client - Numero Siren: ",
@@ -50,100 +51,151 @@ class Command(BaseCommand):
 
         elif options['read']:
             client_name = input("Nom du client: ")
-            client_id = get_ee_client_id(client_name=client_name)
-            if client_id is None:
-                print_command_result("Impossible de trouver ce client")
+            client = get_object_from_field_name(
+                view_url='client',
+                filter_field_name='name',
+                filter_field_value=client_name,
+                )
+
+            if client is None:
+                print_command_result(ERROR_MESSAGE['client_not_existing'])
                 return
 
-            result = self.client_read(client_id=client_id)
+            # formatting event for terminal output
+            event_info = []
+            for line in CLIENT_FIELD_LIST:
+                if line == 'ee_contact':
+                    user = get_object_from_field_name(
+                        filter_field_name='id',
+                        filter_field_value=client['ee_contact'],
+                        view_url='user'
+                    )
+                    event_info.append(f"{CLIENT_DESCRIPTION[line]} {user['username']}")
+                else:
+                    event_info.append(f"{CLIENT_DESCRIPTION[line]} {client[line]}")
 
-            client_info = []
-            client_info.append(f"Client: {result['name']}")
-            client_info.append(f"Epic Events - Contact: {result['ee_contact_name']}")
-            client_info.append(f"Client - Numero Siren: {result['siren']}")
-            client_info.append(f"Client - Nom du contact: {result['client_contact_name']}")
-            client_info.append(f"Client - Email: {result['email']}")
-            client_info.append(f"Client - Téléphone: {result['phone']}")
-            client_info.append(f"Client - Information: {result['information']}")
-
-            print_command_result(printable=client_info)
+            print_command_result(printable=event_info)
 
             return
 
         elif options['create']:
-            # ask contact name and get contact id from it
-            ee_contact_name = input('Epic Events - Nom du contact: ')
-            ee_contact_id = get_ee_contact_id(ee_contact_name)
-
-            if ee_contact_id is None:
-                print("Impossible de trouver cet utilisateur dans la base de données.")
-                return
-
-            client_data = {
-                'ee_contact': ee_contact_id,
-                'name': "",
-                'siren': "",
-                'client_contact_name': "",
-                'email': "",
-                'phone': "",
-                'information':""
-            }
+            client_field_list_create = CLIENT_FIELD_LIST
 
             # gets client's info from user's input
-            for line in CLIENT_FIELD_LIST:
-                user_input = input(CLIENT_DESCRIPTION[line])
-                input_valid = input_validated(user_input)
+            client_data = {}
+            for line in client_field_list_create:
+                if line == 'ee_contact':
+                    user = get_object_from_field_name(
+                        filter_field_name='username',
+                        filter_field_value=input(CLIENT_DESCRIPTION[line]),
+                        view_url='user'
+                    )
+                    if  user is None:
+                        print(ERROR_MESSAGE['user_not_existing'])
+                        return None
+
+                    client_input = user['id']
+                elif 'date' in line:
+                    print(CLIENT_DESCRIPTION[line])
+                    client_input = str(get_date_time_from_user())
+                else:
+                    client_input = input(CLIENT_DESCRIPTION[line])
+
+                input_valid = input_validated(client_input)
                 if not input_valid:
                     return
-                client_data[line] = user_input
+                client_data[line] = client_input
 
             result = self.client_create(client_data)
 
-            if result is None:
-                print_command_result('Impossible de créer ce client.')
+            if result[-1]['response_status'] // 100 != 2 :
+                print_command_result('Impossible de créer cet événement.')
+                print(result['response_text'])
             else:
-                print_command_result(f"Client '{client_data['name']}' créé avec succès")
+                print_command_result(f"Evénement '{client_data['name']}' créé avec succès")
 
         elif options['delete']:
-            client_name = input("Nom du client à supprimer: ")
-            client_id = get_ee_client_id(client_name=client_name)
+            client_name = input("Information de l'événement à supprimer: ")
+            client = get_object_from_field_name(
+                filter_field_name='name',
+                filter_field_value=client_name,
+                view_url='client'
+                )
 
-            if client_id is None:
-                print("Impossible de trouver ce client dans la base de données.")
+            if client is None:
+                print(ERROR_MESSAGE['client_not_existing'])
                 return
 
-            result = self.client_delete(client_id=client_id)
-            if result is None:
+            result = self.client_delete(client_id=client['id'])
+
+            if result[-1]['response_status'] // 100 != 2 :
                 print_command_result("Impossible de supprimer ce client")
             else:
                 print_command_result(f"'{client_name}' supprimé avec succès.")
 
         elif options['update']:
             client_name = input("Nom du client à modifier: ")
-            client_id = get_ee_client_id(client_name=client_name)
+            client_data = get_object_from_field_name(
+                filter_field_name='name',
+                filter_field_value=client_name,
+                view_url='client'
+                )
 
-            if client_id is None:
-                print("Impossible de trouver ce client dans la base de données.")
+            if client_data is None:
+                print(ERROR_MESSAGE['client_not_existing'])
                 return
 
-            # get client data
-            client_data = self.client_read(client_id)
             client_description = CLIENT_DESCRIPTION
             print('Entrer les valeurs à mettre à jour, laisser vide pour garder les existantes:')
+
             for line in CLIENT_FIELD_LIST:
-                client_description[line] = client_description[line].replace(':',f' ({client_data[line]}):' )
-                user_input = input(client_description[line])
+                if line == 'ee_contact':
+                    user = get_object_from_field_name(
+                        filter_field_name='id',
+                        filter_field_value=client_data['ee_contact'],
+                        view_url='user'
+                    )
+                    client_description[line] = client_description[line].replace(
+                        ':',f" ({user['username']}):")
+                else:
+                    client_description[line] = client_description[line].replace(
+                        ':',f' ({client_data[line]}):')
 
-                # modify the stored value only if user entered something
-                if user_input != "":
-                    client_data[line] = user_input
+                client_input = input(client_description[line])
 
-            result = self.client_update(client_id=client_id, client_data=client_data)
-            if result is None:
+                # modify the stored value only if user entered something for this line
+                if client_input != "" and client_data[line] != client_input:
+                    # getting contract & ee_contact id from user's input
+                    if line == 'contract':
+                        contract = get_object_from_field_name(
+                            filter_field_name='name',
+                            filter_field_value=client_input,
+                            view_url='contract'
+                            )
+                        if contract is None:
+                            print_command_result(ERROR_MESSAGE['contract_not_existing'])
+                            return
+                        client_data[line] = contract['id']
+                    elif line == 'ee_contact':
+                        user = get_object_from_field_name(
+                            filter_field_name='username',
+                            filter_field_value=client_input,
+                            view_url='user'
+                        )
+                        if user is None:
+                            print_command_result(ERROR_MESSAGE['user_not_existing'])
+                            return
+                        client_data[line] = user['id']
+                    else:
+                        client_data[line] = client_input
+
+            result = self.client_update(
+                client_id=client_data['id'], client_data=client_data)
+
+            if result[-1]['response_status'] // 100 != 2 :
                 print_command_result("Impossible de modifier ce client")
             else:
                 print_command_result(f"'{client_name}' modifié avec succès.")
-
 
     def client_list(self):
         """
@@ -151,7 +203,8 @@ class Command(BaseCommand):
         returns a list of client or None
         '"""
         response = request_commands(view_url='client', operation="read")
-        if response is None:
+        response_data = response.pop(-1)
+        if response_data['response_status'] != 200 :
             return None
 
         client_list = []
